fc1608df27296ec82ba14e4d2e38d21b85962054
stackprobe <stackprobes@gmail.com>
2020-05-10 01:39:34 +0900

    Tender

--- PutFile/PutFile/Kernel.h
+++ PutFile/PutFile/Kernel.h
@@ -1,5 +1,10 @@
+// sync > @ updateDiskSpace_h
+
+extern uint64 lastDiskFree_User; // このプロセスが使用出来る空き領域サイズ
 extern uint64 lastDiskFree;
-extern uint64 lastDiskFree_real;
 extern uint64 lastDiskSize;
 
+void updateDiskSpace(int drive);
 void updateDiskSpace_Dir(char *dir);
+
+// < sync
